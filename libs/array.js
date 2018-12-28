c7d1dbd9030a3418c66399cb7fc05c4840d0c27c
刘卿 <lqing@jointwisdom.cn>
2018-12-28 09:54:38 +0800

    add: util

--- libs/array.js
+++ libs/array.js
@@ -1,8 +1,20 @@
 /**
  * 是否数组
- * @param {Date} dirtyArray 数组对象
+ * @param {Array} dirtyArray 数组对象
  * @returns {Boolean} 是否数组类型
  */
 exports.is = (dirtyArray) => {
     return Object.prototype.toString.call(dirtyArray) === '[object Array]';
 }
+/**
+ * 按字符对数组进行排序
+ * @param {Array} dirtyArray 需要排序的数组
+ * @returns {Array} 新数组
+ */
+exports.sortCharacters = dirtyArray =>{
+    return dirtyArray.sort((a, b) => {
+        return a.localeCompare(b)
+    });
+}
+
+
---
+++ libs/other.js
@@ -0,0 +1,48 @@
+/**
+ * query解析为object
+ * @param {String} query 需要解析的query/url
+ * @returns {Object} 新数组
+ */
+exports.queryToObj = (query) => {
+    const queryArr = query.split('?');
+    const url = queryArr[0];
+    const search = queryArr[1];
+    if(!search){
+        return {
+            url
+        }
+    }
+
+    const handle = decodeURIComponent(search).split('&').filter((item) => item !== '');
+
+    const obj = {
+        url,
+        search,
+        query: {}
+    }
+
+    handle.forEach((item) => {
+        const temp = item.split('=');
+        obj.query[temp[0]] = temp[1];
+    });
+
+    return obj;
+}
+/**
+ * object解析为query
+ * @param {Object} obj query Object 
+ * @returns {String} query字符串
+ */
+exports.objToQuery = (obj) => {
+    const url = obj.url ? '?' + obj.url : '';
+    let search = obj.search;
+    const query = obj.query
+    if(!search && query){
+        const tempQueryArr = [];
+        for(let key in query){
+            tempQueryArr.push(`${key}=${query[key]}`);
+        }
+        search = tempQueryArr.join('&');
+    }
+    return `${url}${search}`;
+}
